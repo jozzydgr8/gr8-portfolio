6bdb4fb308c75611dfe39e43cfd0604bdbe80ce1
jozzydgr8 <jozzydgr8@gmail.com>
2023-09-24 22:30:59 +0100

    added cv

--- src/Header/Headerbutton.jsx
+++ src/Header/Headerbutton.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './header.css';
+import CV from '../assets/cv.pdf'
 
 class Headerbutton extends Component {
   render() {
     return (
       
         <div className = 'header-button'>
-        <a href=""><button className='btn cv'>Download CV</button></a>
+        <a href={CV} download={CV}><button className='btn cv'>Download CV</button></a>
         <a href="https://wa.link/jlaiwr" target='_blank'><button className='btn talk'>Let's talk</button></a>
       </div>
       
