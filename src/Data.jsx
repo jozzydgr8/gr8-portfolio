375c0acf5c1919f4e668c0c923da6dfa0bf30042
jozzydgr8 <jozzydgreat1@gmail.com>
2024-11-11 16:05:24 -0800

    update taystychow to portfolio removing medical

---
+++ src/Data.jsx
@@ -0,0 +1,26 @@
+import image1 from './assets/Portfolio/apartment web overview.jpeg';
+import image3 from './assets/Portfolio/ejuandy overview.jpeg';
+import image2 from './assets/Portfolio/Taysty zobo.jpeg'
+export const Data = [
+    {
+        id:'1',
+        image:image1,
+        title:"Mag's Resident",
+        desc:'A functional apartment website that combines clean design with intuitive navigation and responsiveness(mobile).',
+        live:'https://jozzydgr8.github.io/apartments/'
+    },
+    {
+        id:'2',
+        title:'Taysty Chow',
+        image:image2,
+        desc:'A responsive e-commerce website for a small pastry business called taysty chow, showcasing an interactive menu for users to explore various pastry offerings.',
+        live:'https://jozzydgr8.github.io/tayschow/'
+    },
+    {
+        id:'3',
+        image:image3,
+        title:'Ejuandy',
+        desc:'A comprehensive and interactive web application to showcase information and details of a communications and entertainment consultancy company in Nigeria',
+        live:'https://jozzydgr8.github.io/ejuandy/'
+    }
+]
--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import { FaGithub } from "react-icons/fa";
 import './portfolio.css';
-import image1 from '../assets/Portfolio/WhatsApp Image 2024-10-01 at 07.52.35.jpeg';
-import image2 from '../assets/Portfolio/WhatsApp Image 2024-10-01 at 07.52.36.jpeg';
-import image3 from '../assets/Portfolio/WhatsApp Image 2024-10-01 at 07.52.52.jpeg'
-
-
-
-
-const data =[]
+import {Data }from '../Data'
 
 
 class Portfolio extends Component {
@@ -21,59 +14,30 @@ class Portfolio extends Component {
                 <h2 className='alternate'>Projects</h2>
                 <div className="portfolio-container">
                   
-                  
+                  {
+                    Data && Data.map(data=>(
+                      <article key={data.id}>
+                        
+                        <img className='portfolio-image' src={data.image} alt="" />
+                            
+                            <div>
+                            <h3>{data.title}</h3>
+    
+                            <div>
+                            {data.desc}
+                            </div>
+                            <a href={data.live} target='_blank'>
+                            <button className='btn btn-md portfolio-button '>Preview</button>
+                            </a>
+                            </div>
+                        </article>
+                    ))
+                  }
                     
-                      <article>
-                      <img className='portfolio-image' src={image2} alt="" />
-                          
-                          <div>
-                          <h3>Mag's Resident</h3>
-  
-                          <div>
-                          A functional apartment website that combines clean design with intuitive navigation and 
-                          responsiveness(mobile).
-                          </div>
-                          <a href='https://jozzydgr8.github.io/apartments/' target='_blank'>
-                          <button className='btn btn-md portfolio-button '>Preview</button>
-                          </a>
-                          </div>
-                      </article>
-
-
-                      <article>
-                      <img className='portfolio-image' src={image3} alt="" />
                       
-                          <div>
-                          <h3>Medical E-commerce website</h3>
-                          <div>An interactive and secure e-commerce
-                          platform for medical equipment’s
-                          </div>
-                          <br/>
-  
+
+
                       
-                          <a href='https://jozzydgr8.github.io/medical/' target='_blank'>
-                          <button className='btn btn-md portfolio-button '>Preview</button>
-                          </a>
-                          </div>
-                      </article>
-
-
-                      <article>
-                      <img className='portfolio-image' src={image1} alt="" />
-
-                          <div>
-                          <h3>Ejuandy </h3>
-                          
-                          <div>
-                          A comprehensive and interactive web application to showcase information and details of a communications and 
-                          entertainment consultancy company in Nigeria
-                          </div>
-                      
-                          <a href='https://jozzydgr8.github.io/ejuandy/' target='_blank'>
-                          <button className='btn btn-md portfolio-button '>Preview</button>
-                          </a>
-                          </div>
-                      </article>
                   
                   
                 </div>
